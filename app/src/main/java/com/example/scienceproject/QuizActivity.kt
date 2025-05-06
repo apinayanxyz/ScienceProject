dcbde8f38cab8077aa8eab964a1955acce0c23ee
apinayanxyz <akanenthirarasa@gmail.com>
2025-05-06 17:48:45 +0100

    Version-0.1.0 made buttons send user to different pages

--- app/src/main/java/com/example/scienceproject/QuizActivity.kt
+++ app/src/main/java/com/example/scienceproject/QuizActivity.kt
@@ -11,6 +11,7 @@ class QuizActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_quiz)
+        supportActionBar?.hide();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/scienceproject/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/scienceproject/ui/home/HomeFragment.kt
@@ -1,13 +1,19 @@
 package com.example.scienceproject.ui.home
 
 import android.app.ActionBar
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.scienceproject.MainActivity
+import com.example.scienceproject.QuizActivity
+import com.example.scienceproject.R
+import com.example.scienceproject.TestActivity
 import com.example.scienceproject.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -23,17 +29,76 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
 
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        var physicsQuizButton = view.findViewById<Button>(R.id.physicsQuizButton)
+        var biologyQuizButton = view.findViewById<Button>(R.id.biologyQuizButton)
+        var chemistyQuizButton = view.findViewById<Button>(R.id.chemistryQuizButton)
+        var allQuizButton = view.findViewById<Button>(R.id.allQuizButton)
+        var randomQuizButton = view.findViewById<Button>(R.id.randomQuizButton)
+
+        var physicsTestButton = view.findViewById<Button>(R.id.physicsTestButton)
+        var biologyTestButton = view.findViewById<Button>(R.id.biologyTestButton)
+        var chemistyTestButton = view.findViewById<Button>(R.id.chemistryTestButton)
+        var allTestButton = view.findViewById<Button>(R.id.allTestButton)
+        var randomTestButton = view.findViewById<Button>(R.id.randomTestButton)
+
+        val quizIntent = Intent(requireContext(),QuizActivity::class.java)
+        val testIntent = Intent(requireContext(),TestActivity::class.java)
+
+
+        physicsQuizButton.setOnClickListener {
+            quizIntent.putExtra("testType",1)
+            startActivity(quizIntent)
+        }
+        biologyQuizButton.setOnClickListener {
+            quizIntent.putExtra("testType",2)
+            startActivity(quizIntent)
+        }
+        chemistyQuizButton.setOnClickListener {
+            quizIntent.putExtra("testType",3)
+            startActivity(quizIntent)
+        }
+        allQuizButton.setOnClickListener {
+            quizIntent.putExtra("testType",0)
+            startActivity(quizIntent)
+        }
+        randomQuizButton.setOnClickListener {
+            quizIntent.putExtra("testType",4)
+            startActivity(quizIntent)
+        }
+
+        physicsTestButton.setOnClickListener {
+            testIntent.putExtra("testType",1)
+            startActivity(testIntent)
+        }
+        biologyTestButton.setOnClickListener {
+            testIntent.putExtra("testType",2)
+            startActivity(testIntent)
+        }
+        chemistyTestButton.setOnClickListener {
+            testIntent.putExtra("testType",3)
+            startActivity(testIntent)
+        }
+        allTestButton.setOnClickListener {
+            testIntent.putExtra("testType",0)
+            startActivity(testIntent)
+        }
+        randomTestButton.setOnClickListener {
+            testIntent.putExtra("testType",4)
+            startActivity(testIntent)
+        }
+
+    }
+    //fun buttonClick(view:View?)
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null

fd1556bb5b24c19f0794805b8f1d8e738b305ca1
apinayanxyz <akanenthirarasa@gmail.com>
2025-05-09 00:07:51 +0100

    Version-0.1.7 Added dummy questions to QuestionList Modified question template to add hint and question type added functions to QuestionList

--- app/src/main/java/com/example/scienceproject/questionProcess/QuestionList.kt
+++ app/src/main/java/com/example/scienceproject/questionProcess/QuestionList.kt
@@ -1,7 +1,53 @@
 package com.example.scienceproject.questionProcess
 
 class QuestionList {
-    var questionList = listOf(
-        Questions(1,1,"",1,"","","","")
-    )
+
+
+        /*Question Type
+        * 0 is multiple choice
+        * */
+        var questionList = listOf(
+        Questions(1,1,"Question 1",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(2,1,"Question 2",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(3,1,"Question 3",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(4,1,"Question 4",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(5,1,"Question 5",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(6,1,"Question 6",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(7,1,"Question 7",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(8,1,"Question 8",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(9,1,"Question 9",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(10,1,"Question 10",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(11,2,"Question 1",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(12,2,"Question 2",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(13,2,"Question 3",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(14,2,"Question 4",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(15,2,"Question 5",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(16,2,"Question 6",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(17,2,"Question 7",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(18,2,"Question 8",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(19,2,"Question 9",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(20,2,"Question 10",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(21,3,"Question 1",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(22,3,"Question 2",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(23,3,"Question 3",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(24,3,"Question 4",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(25,3,"Question 5",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(26,3,"Question 6",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(27,3,"Question 7",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(28,3,"Question 8",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(29,3,"Question 9",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+        Questions(30,3,"Question 10",1,"Answer 1","Answer 2","Answer 3","Answer 4",0,"Hint"),
+
+        )
+    fun shuffleList(): List<Questions> {
+        return questionList.shuffled()
+    }
+
+    fun createQuestions(numberOfQuestions: Int):List<Questions>{
+        var smallerQuestionList = mutableListOf<Questions>()
+        for (i in 0..numberOfQuestions){
+            smallerQuestionList.add(i,questionList[i])
+        }
+        return smallerQuestionList
+    }
 }
--- app/src/main/java/com/example/scienceproject/questionProcess/Questions.kt
+++ app/src/main/java/com/example/scienceproject/questionProcess/Questions.kt
@@ -2,7 +2,7 @@ package com.example.scienceproject.questionProcess
 
 import javax.security.auth.Subject
 
-class Questions(id:Int,subject: Int,question:String,correctAnswer:Int,answer1:String,answer2:String,answer3:String,answer4:String) {
+class Questions(id:Int,subject: Int,question:String,correctAnswer:Int,answer1:String,answer2:String,answer3:String,answer4:String,questionType:Int,questionHint:String) {
     var questionId : Int? = id
     var questionSubject: Int? = subject
     var question : String? = question
@@ -11,6 +11,7 @@ class Questions(id:Int,subject: Int,question:String,correctAnswer:Int,answer1:St
     var answer2 : String? = answer2
     var answer3 : String? = answer3
     var answer4 : String? = answer4
-
+    var questionType : Int? = questionType
+    var questionHint : String = questionHint
 
 }

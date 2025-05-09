c6bd4b243020ab32be5ed272491823e1ffd7546a
apinayanxyz <akanenthirarasa@gmail.com>
2025-05-09 01:25:12 +0100

    Version-0.1.10 made questions layout appear dynamically based on question information

--- app/src/main/java/com/example/scienceproject/questionProcess/QuestionsActivity.kt
+++ app/src/main/java/com/example/scienceproject/questionProcess/QuestionsActivity.kt
@@ -15,8 +15,7 @@ import com.example.scienceproject.R
 import org.w3c.dom.Text
 
 class QuestionsActivity : AppCompatActivity() {
-    val questionId = 0;
-
+    //Data variables
     private var questionNumber:Int? =null
     private var maxQuestions:Int? = null
     private var timeRemaining:Long? = null
@@ -35,35 +34,13 @@ class QuestionsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val answerOneButton = findViewById<Button>(R.id.answerOne)
-        val answerTwoButton = findViewById<Button>(R.id.answerTwo)
-        val answerFourButton = findViewById<Button>(R.id.answerThree)
-        val answerThreeButton = findViewById<Button>(R.id.answerFour)
-
-        val questionNumberText = findViewById<TextView>(R.id.questionNumberLabel)
-        val subjectText = findViewById<TextView>(R.id.subjectLabel)
-        var timerText = findViewById<TextView>(R.id.timerLabel)
-        val questionText = findViewById<TextView>(R.id.questionLabel)
-        var nextButton = findViewById<TextView>(R.id.nextButton)
-
-        var layout = findViewById<FrameLayout>(R.id.backgroundLayout)
-
         val extras = intent.extras;
-        if (extras != null) {
-            questionNumber = extras.getInt("questionNumber")
-            maxQuestions = extras.getInt("maxQuestions")
-            timeRemaining = extras.getLong("timeLimit")
-            subject = extras.getInt("subject")
-            score = extras.getInt("score")
-            questionListCreator = extras.getSerializable("questionList") as? QuestionList
-            questionList = questionListCreator?.questionList
-            when(questionList?.get(questionNumber!!)?.questionSubject){
-                1->layout.background = resources.getColor(R.color.physicsColor).toDrawable()
-                2->layout.background = resources.getColor(R.color.biologyColor).toDrawable()
-                3->layout.background = resources.getColor(R.color.chemistryGreen).toDrawable()
-            }
-        }
+        layoutOfQuestion(extras)
+        timerCreation(extras)
+    }
 
+    private fun timerCreation(extras: Bundle?) {
+        var timerText: TextView = findViewById<TextView>(R.id.timerLabel)
         object : CountDownTimer (timeRemaining!!,1000){
             override fun onTick(millisUntilFinished: Long) {
                 timerText.text = ""+(millisUntilFinished / 1000)/60 +":"+(millisUntilFinished / 1000)%60
@@ -75,5 +52,73 @@ class QuestionsActivity : AppCompatActivity() {
                 timerText.text = "done!"
             }
         }.start()
+    }
+
+    private fun layoutOfQuestion(extras:Bundle?){
+        //Buttons for answering
+        val answerOneButton: Button = findViewById<Button>(R.id.answerOne)
+        val answerTwoButton: Button = findViewById<Button>(R.id.answerTwo)
+        val answerFourButton: Button = findViewById<Button>(R.id.answerThree)
+        val answerThreeButton: Button = findViewById<Button>(R.id.answerFour)
+
+        //Text on screen and key layout parts
+        val questionNumberText: TextView = findViewById<TextView>(R.id.questionNumberLabel)
+        val subjectText: TextView = findViewById<TextView>(R.id.subjectLabel)
+        var timerText: TextView = findViewById<TextView>(R.id.timerLabel)
+        val questionText: TextView = findViewById<TextView>(R.id.questionLabel)
+        var nextButton: TextView = findViewById<TextView>(R.id.nextButton)
+
+        //Background and other useful information
+        var layout: FrameLayout = findViewById<FrameLayout>(R.id.backgroundLayout)
+
+        if (extras != null) {
+            questionNumber = extras.getInt("questionNumber")
+            maxQuestions = extras.getInt("maxQuestions")
+            timeRemaining = extras.getLong("timeLimit")
+            subject = extras.getInt("subject")
+            score = extras.getInt("score")
+            questionListCreator = extras.getSerializable("questionList") as? QuestionList
+            questionList = questionListCreator?.questionList
+
+            //Add Text to non buttons
+            //--//
+            //Question number
+            questionNumberText.text=(questionNumber!! +1).toString()
+            //Subject of question
+            subjectText.text=when(questionList?.get(questionNumber!!)?.questionSubject){
+                1->"Physics"
+                2->"Biology"
+                3->"Chemistry"
+                else->"All"
+            }
+            //The question
+            questionText.text=questionList?.get(questionNumber!!)?.question
+            //--//
+            //Sets text of next button
+            if (questionNumber!! < maxQuestions!!){
+                nextButton.text="Next"
+            }
+            else{
+                nextButton.text="Finish"
+            }
+
+            //Text on answering buttons
+            //--//
+            //Button 1
+            answerOneButton.text= questionList?.get(questionNumber!!)?.answer1
+            //Button 2
+            answerTwoButton.text= questionList?.get(questionNumber!!)?.answer2
+            //Button 3
+            answerThreeButton.text= questionList?.get(questionNumber!!)?.answer3
+            //Button 4
+            answerFourButton.text= questionList?.get(questionNumber!!)?.answer4
+            //--//
+            //Color Background
+            when(questionList?.get(questionNumber!!)?.questionSubject){
+                1->layout.background = resources.getColor(R.color.physicsColor).toDrawable()
+                2->layout.background = resources.getColor(R.color.biologyColor).toDrawable()
+                3->layout.background = resources.getColor(R.color.chemistryGreen).toDrawable()
+            }
+        }
     }
 }
